970524e2b9d21f95b073b9ead7be97a4ef33ae2d
Sarah Li <sarahli@sarahs-air-3.wifi.local.cmu.edu>
2024-02-03 08:51:26 -0500

    hamburger menu

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Home page that renders the map view
 @app.route('/')
-def map_view():
+def index():
     # Fetch your routes data from your data source here
     # routes_data = #INSERT ROUTE DATA HERE
     return render_template('map_view.html')
@@ -16,6 +16,6 @@ def map_view():
 def routes():
     return jsonify(all_routes)
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     app.run(debug=True)
 
